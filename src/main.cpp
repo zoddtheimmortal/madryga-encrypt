47b8240091dea0ec53a761e29d1e01ea37843477
Alok Desai <alokdesai2004@gmail.com>
2024-04-12 23:01:43 +0530

    feat: add madryga encrypt and decrypt

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,35 @@
 #include <vector>
 #include <filesystem>
 
-uint32_t madryga_encrypt(uint32_t plain_text, uint32_t key, int num_rounds)
+#define ROUNDS 4
+
+const uint32_t delta = 0x9e3779b9;
+
+uint32_t madryga_encrypt(uint32_t plain_text, uint32_t key)
 {
-    return NULL;
+    uint32_t v0 = plain_text, v1 = key, sum = 0;
+    for (int i = 0; i < ROUNDS; i++)
+    {
+        sum += delta;
+        v0 += ((v1 << 4) + key) ^ (v1 + sum) ^ ((v1 >> 5) + key);
+        v1 += ((v0 << 4) + key) ^ (v0 + sum) ^ ((v0 >> 5) + key);
+    }
+    return v0;
 }
 
-uint32_t madryga_decrypt(uint32_t cipher_text, uint32_t key, int num_rounds)
+uint32_t madryga_decrypt(uint32_t cipher_text, uint32_t key)
 {
-    return NULL;
+    uint32_t v0 = cipher_text, v1 = key, sum = delta * ROUNDS;
+    for (int i = 0; i < ROUNDS; i++)
+    {
+        v1 -= ((v0 << 4) + key) ^ (v0 + sum) ^ ((v0 >> 5) + key);
+        v0 -= ((v1 << 4) + key) ^ (v1 + sum) ^ ((v1 >> 5) + key);
+        sum -= delta;
+    }
+    return v0;
 }
 
-void encrypt_directory(std::string &directory_path, std::string key, int num_rounds)
+void encrypt_directory(std::string &directory_path, std::string key)
 {
     for (const auto &entry : std::filesystem::directory_iterator(directory_path))
     {
@@ -42,7 +60,7 @@ void encrypt_directory(std::string &directory_path, std::string key, int num_rou
         }
     }
 
-    void decrypt_directory(std::string & directory_path, std::string key, int num_rounds)
+    void decrypt_directory(std::string & directory_path, std::string key)
     {
         return;
     }
@@ -52,15 +70,14 @@ void encrypt_directory(std::string &directory_path, std::string key, int num_rou
         std::string directory_path, key;
         int num_rounds;
 
-        if (argc != 4)
+        if (argc != 3)
         {
-            std::cerr << "Usage: " << argv[0] << " <directory_path> <key> <num_rounds>\n";
+            std::cerr << "Usage: " << argv[0] << " <directory_path> <key>\n";
             return 1;
         }
 
         directory_path = argv[1];
         key = argv[2];
-        num_rounds = std::stoi(argv[3]);
 
         int choice;
         std::cout << "1. Encrypt\n2. Decrypt\n";
@@ -68,11 +85,11 @@ void encrypt_directory(std::string &directory_path, std::string key, int num_rou
 
         if (choice == 1)
         {
-            encrypt_directory(directory_path, key, num_rounds);
+            encrypt_directory(directory_path, key);
         }
         else if (choice == 2)
         {
-            decrypt_directory(directory_path, key, num_rounds);
+            decrypt_directory(directory_path, key);
         }
         else
         {
